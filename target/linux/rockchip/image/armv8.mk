f5744980421d7f521ff215dfe6f97d1f73461fdc
aguoxing <guoxing98@foxmail.com>
2025-01-09 17:40:49 +0800

    rockchip: add fine3399 support

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -98,6 +98,16 @@ define Device/friendlyarm_nanopc-t4
 endef
 TARGET_DEVICES += friendlyarm_nanopc-t4
 
+define Device/rumu3f_fine-3399
+  DEVICE_VENDOR := RUMU3F
+  DEVICE_MODEL := FINE 3399
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := fine3399-rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-gpio-button-hotplug kmod-r8168
+endef
+TARGET_DEVICES += rumu3f_fine-3399
+
 define Device/friendlyarm_nanopc-t6
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPC T6
