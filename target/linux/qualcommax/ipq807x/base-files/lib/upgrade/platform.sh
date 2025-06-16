b7b2cb0c14964e9f4fc08ae080b9b341d9fdd9fc
guoxing <guoxing98@foxmail.com>
2025-06-16 10:49:23 +0800

    [200~qualcommax: ipq807x: add support for TCL HH500V TCL HH500V also known as Vodafone Gigacube B157 is a dual band 802.11ax 5G NR CPE with an FXS port.
    Specifications:
    * SoC: Qualcomm IPQ8072A
    * RAM: 1024 MB
    * NAND flash: 256MB NAND (MT29F2G08ABBGAH4)
    * NOR flash: 64MB NOR (MX25U25635F) - only in some variants
    * WiFi 5G: Qualcomm QCN5054
    * WiFi 2.4G: Qualcomm QCN5024
    * Ethernet 2.5G: Qualcomm QCA8081
    * Ethernet 1G: Qualcomm QCA8075
    * Modem: GosunCn GM800 (Qualcomm Snapdragon X55)
    * SIM: 1 nano-SIM card slot
    * Buttons: Power, Reset, WPS
    * LEDs: Power (B/R), WiFi (B), 4G (B/Y/R), 5G (B/Y/R)
    * VoIP: 1 FXS RJ11 port
    * Power: 12V, 3A
    
    UART serial console:
    
    * 115200,8N1,1.8V
    * Three unmarked test points next to QJ2012A:
    
     +---------+
     | QJ2012A |
     +---------+
     o TX
     o GND
     o RX
     o
     o
     o
    
    Installation via OEM firmware:
    
    * Use the following process to obtain root ssh access
    https://forum.openwrt.org/t/openwrt-support-for-vodafone-gigacube-b157/150371/24
    * Connect using root with no password on port 42000
    * Optionally for serial bootloader access run :
    fw_setenv bootdelay=3
    * SCP factory.ubi to /tmp
    * Run:
    sysupgrade -n /tmp/factory.ubi
    
    Installation via serial console and OEM firmware::
    
    * Use console to access OEM firmware shell
    * Proceed with SCP & sysupgrade as described aboe
    
    Other notes:
    
    * This device uses active partition rotation
    * Some versions (TCL branded?) have a NOR chip in addition to NAND
    * The above is supported by u-boot patching DT partitions
    * DT patching does NOT occur on tftpboot/bootm
    ~

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -289,6 +289,10 @@ platform_do_upgrade() {
 		CI_DATAPART="rootfs_data"
 		emmc_do_upgrade "$1"
 		;;
+  tcl,hh500v)Add commentMore actions
+		tcl_upgrade_prepare
+		nand_do_upgrade "$1"
+		;;
 	tplink,deco-x80-5g|\
 	tplink,eap620hd-v1|\
 	tplink,eap660hd-v1)
